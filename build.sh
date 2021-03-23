08181161b5f875e2b7ee3eab13f63955ea714f45
pedrozzz0 <guitopzika26@gmail.com>
2021-03-23 16:29:24 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 # Thanks to bdashore3 @ GitHub
 # Thanks to Draco (tytydraco @ GitHub)
 
+bold=$(tput bold)
+default=$(tput sgr0)
 date=`date`
 version=`cat module.prop | grep version= | sed "s/version=//"`
 
@@ -14,7 +16,7 @@ echo "builddate=$date
 buildtype=$build_type
 codename=$codename" > $(pwd)/info.prop
  
-echo "Zipping KTSR-$version-$build_type-$codename..."
+echo "Zipping ${bold}KTSR-$version-$build_type-$codename${default}..."
 
 zip -r "KTSR-$version-$build_type-$codename.zip" . -x *.git* -x *.zip -x C1.sh -x kingauto -x kingtweaks -x *.apk -x *.bak -x libktsr.sh -x kcal.sh -x build.sh
 
@@ -22,4 +24,9 @@ mv "KTSR-$version-$build_type-$codename.zip" ../out
 
 rm $(pwd)/info.prop
 
-echo "Done, check the folder to the finished build."
+if [ $? -ne 1 ]; then
+echo "${bold}Build done${default}, check the folder to the finished build."
+
+else
+echo "${bold}Build failed!${default}, please fix the error(s) and try again."
+fi
