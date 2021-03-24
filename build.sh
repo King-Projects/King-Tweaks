f06fe229c7c8fdef65b37eec9be384e10ad28eb6
pedrozzz0 <guitopzika26@gmail.com>
2021-03-24 19:38:18 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,14 @@
 # Thanks to bdashore3 @ GitHub
 # Thanks to Draco (tytydraco @ GitHub)
 
+yellow=$(tput setaf 3)
+green=$(tput setaf 2)
+boldgreen=${bold}$green
+boldred=${bold}$red
+red=$(tput setaf 1)
+blue=$(tput setaf 4)
 bold=$(tput bold)
+blink=$(tput blink)
 default=$(tput sgr0)
 date=`date`
 version=`cat module.prop | grep version= | sed "s/version=//"`
@@ -16,7 +23,7 @@ echo "builddate=$date
 buildtype=$build_type
 codename=$codename" > $(pwd)/info.prop
  
-echo "Zipping ${bold}KTSR-$version-$build_type-$codename${default}..."
+echo "Zipping ${blink}KTSR-$version-$build_type-$codename${default}..."
 
 zip -r "KTSR-$version-$build_type-$codename.zip" . -x *.git* -x *.zip -x C1.sh -x kingauto -x kingtweaks -x *.apk -x *.bak -x libktsr.sh -x kcal.sh -x build.sh
 
@@ -25,8 +32,8 @@ mv "KTSR-$version-$build_type-$codename.zip" ../out
 rm $(pwd)/info.prop
 
 if [ $? -ne 1 ]; then
-echo "${bold}Build done${default}, check the folder to the finished build."
+echo "${boldgreen}Build done${blue}, check the folder to the finished build."
 
 else
-echo "${bold}Build failed!${default}, please fix the error(s) and try again."
+echo "${boldred}Build failed!${yellow}, please fix the error(s) and try again."
 fi
