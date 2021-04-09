f95e688780f6c779d1161ef7ebe056fe71c12966
pedrozzz0 <guitopzika26@gmail.com>
2021-04-08 22:44:23 -0300

    Minor improvements
    Signed-off-by: pedrozzz0 <guitopzika26@gmail.com>

--- kcal.sh
+++ kcal.sh
@@ -1,9 +1,5 @@
 #!/system/bin/sh
 
-MODPATH=/data/adb/modules/KTSR
-
-. $MODPATH/libs/libktsr.sh
-
 if [[ -e /sdcard/KTSR/kxvision.log ]]; then
 rm /sdcard/KTSR/kxvision.log
 fi
@@ -16,9 +12,9 @@ write "/sys/devices/platform/kcal_ctrl.0/kcal_val" "253"
 write "/sys/devices/platform/kcal_ctrl.0/kcal_cont" "258"
 
 if [[ $? == "1" ]]; then
-kmsg1 "[!] Kcal preset executed with errors." > /sdcard/KTSR/kxvision.log
+echo -e "[!] Kcal preset executed with errors." > /sdcard/KTSR/kxvision.log
 exit 1
 else
-kmsg "Kcal preset executed without any errors! Enjoy." > /sdcard/KTSR/kxvision.log
+echo -e "[*] Kcal preset executed without any errors! Enjoy." > /sdcard/KTSR/kxvision.log
 exit 0
 fi
