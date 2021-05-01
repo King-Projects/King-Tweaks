58926053ff8554319fe39c128f89db598f9442cb
pedrozzz0 <guitopzika26@gmail.com>
2021-05-01 19:01:50 -0300

    Switch kcal.sh to echo instead of write
    Signed-off-by: pedrozzz0 <guitopzika26@gmail.com>

--- kcal.sh
+++ kcal.sh
@@ -6,11 +6,11 @@ rm "/sdcard/KTSR/kxvision.log"
 fi
 
 # King Xvision 
-write "/sys/devices/platform/kcal_ctrl.0/kcal_enable" "1"
-write "/sys/devices/platform/kcal_ctrl.0/kcal" "236 238 240"
-write "/sys/devices/platform/kcal_ctrl.0/kcal_sat" "275"
-write "/sys/devices/platform/kcal_ctrl.0/kcal_val" "253"
-write "/sys/devices/platform/kcal_ctrl.0/kcal_cont" "258"
+echo "1" > "/sys/devices/platform/kcal_ctrl.0/kcal_enable"
+echo "236 238 240" > "/sys/devices/platform/kcal_ctrl.0/kcal"
+echo "275" > "/sys/devices/platform/kcal_ctrl.0/kcal_sat"
+echo "253" > "/sys/devices/platform/kcal_ctrl.0/kcal_val"
+echo "258" > "/sys/devices/platform/kcal_ctrl.0/kcal_cont"
 
 if [[ $? == "1" ]]; then
 echo "[!] Kcal preset executed with errors." > /sdcard/KTSR/kxvision.log
