48db89c367954d755ba9d2d857a0ca5933844113
Pedrozzz0 <guitopzika26@gmail.com>
2021-05-14 14:14:03 -0300

    Improve mod-util
    Signed-off-by: Pedrozzz0 <guitopzika26@gmail.com>

--- mod-util.sh
+++ mod-util.sh
@@ -2,6 +2,7 @@
 #
 # Terminal Utility Functions
 # by veez21
+# modified by pedrozzz0 
 #
 ##########################################################################################
 
@@ -10,7 +11,7 @@ MODUTILVER=v2.6.1
 MODUTILVCODE=261
 
 # Check A/B slot
-if [ -d /system_root ]; then
+if [[ -d /system_root ]]; then
   isABDevice=true
   SYSTEM=/system_root/system
   SYSTEM2=/system
@@ -21,7 +22,7 @@ else
   SYSTEM2=/system
   CACHELOC=/cache
 fi
-[ -z "$isABDevice" ] && { echo "Something went wrong!"; exit 1; }
+[[ -z "$isABDevice" ]] && { echo "[!] Something went wrong!"; exit 1; }
 
 #=========================== Set Busybox up
 # Variables:
@@ -32,9 +33,9 @@ fi
 # set_busybox <busybox binary>
 # alias busybox applets
 set_busybox() {
-  if [ -x "$1" ]; then
+  if [[ -x "$1" ]]; then
     for i in $(${1} --list); do
-      if [ "$i" != 'echo' ]; then
+      if [[ "$i" != 'echo' ]]; then
         alias "$i"="${1} $i" >/dev/null 2>&1
       fi
     done
@@ -43,23 +44,23 @@ set_busybox() {
   fi
 }
 _busybox=false
-if [ -n $_bb ]; then
+if [[ -n $_bb ]]; then
   true
-elif [ -x $SYSTEM2/xbin/busybox ]; then
+elif [[ -x $SYSTEM2/xbin/busybox ]]; then
   _bb=$SYSTEM2/xbin/busybox
-elif [ -x $SYSTEM2/bin/busybox ]; then
+elif [[ -x $SYSTEM2/bin/busybox ]]; then
   _bb=$SYSTEM2/bin/busybox
 else
-  echo "[!] Busybox not detected"
-  echo "Please install it (@osm0sis busybox recommended)"
+  echo -e "[!] Busybox not detected"
+  echo -e "Please install it (@osm0sis busybox recommended)"
   false
 fi
 set_busybox $_bb
-[ $? -ne 0 ] && exit $?
-[ -n "$ANDROID_SOCKET_adbd" ] && alias clear='echo'
+[[ $? -ne 0 ]] && { echo "[!] Something went wrong!"; exit $?; }
+[[ -n "$ANDROID_SOCKET_adbd" ]] && alias clear='echo'
 _bbname="$($_bb | head -n1 | awk '{print $1,$2}')"
 BBok=true
-if [ "$_bbname" == "" ]; then
+if [[ "$_bbname" == "" ]]; then
   _bbname="Busybox not found!"
   BBok=false
 fi
@@ -70,7 +71,7 @@ fi
 set_perm() {
   chown $2:$3 $1 || return 1
   chmod $4 $1 || return 1
-  (if [ -z $5 ]; then
+  (if [[ -z $5 ]]; then
     case $1 in
       *"system/vendor/app/"*) chcon 'u:object_r:vendor_app_file:s0' $1;;
       *"system/vendor/etc/"*) chcon 'u:object_r:vendor_configs_file:s0' $1;;
@@ -96,7 +97,7 @@ set_perm_recursive() {
 # Mktouch
 mktouch() {
   mkdir -p ${1%/*} 2>/dev/null
-  [ -z $2 ] && touch $1 || echo $2 > $1
+  [[ -z $2 ]] && touch $1 || echo $2 > $1
   chmod 644 $1
 }
 
@@ -105,13 +106,13 @@ grep_prop() {
   local REGEX="s/^$1=//p"
   shift
   local FILES=$@
-  [ -z "$FILES" ] && FILES='/system/build.prop'
+  [[ -z "$FILES" ]] && FILES='/system/build.prop'
   sed -n "$REGEX" $FILES 2>/dev/null | head -n 1
 }
 
 # Is mounted
 is_mounted() {
-  grep -q " `readlink -f $1` " /proc/mounts 2>/dev/null
+  grep -q " $(readlink -f $1) " /proc/mounts 2>/dev/null
   return $?
 }
 
@@ -135,10 +136,10 @@ ABILONG=$(grep_prop ro.product.cpu.abi)
 ARCH=arm
 ARCH32=arm
 IS64BIT=false
-if [ "$ABI" = "x86" ]; then ARCH=x86; ARCH32=x86; fi;
-if [ "$ABI2" = "x86" ]; then ARCH=x86; ARCH32=x86; fi;
-if [ "$ABILONG" = "arm64-v8a" ]; then ARCH=arm64; ARCH32=arm; IS64BIT=true; fi;
-if [ "$ABILONG" = "x86_64" ]; then ARCH=x64; ARCH32=x86; IS64BIT=true; fi;
+if [[ "$ABI" = "x86" ]]; then ARCH=x86; ARCH32=x86; fi;
+if [[ "$ABI2" = "x86" ]]; then ARCH=x86; ARCH32=x86; fi;
+if [[ "$ABILONG" = "arm64-v8a" ]]; then ARCH=arm64; ARCH32=arm; IS64BIT=true; fi;
+if [[ "$ABILONG" = "x86_64" ]]; then ARCH=x64; ARCH32=x86; IS64BIT=true; fi;
   
 # Version Number
 VER=$(grep_prop version $MODDIR/module.prop)
@@ -162,8 +163,8 @@ BGBL='\e[1;30;47m'	# Background W Text Bl
 N='\e[0m'			# How to use (example): echo "${G}example${N}"
 loadBar=' '			# Load UI
 # Remove color codes if -nc or in ADB Shell
-[ -n "$1" -a "$1" == "-nc" ] && shift && NC=true
-[ "$NC" -o -n "$ANDROID_SOCKET_adbd" ] && {
+[[ -n "$1" -a "$1" == "-nc" ]] && shift && NC=true
+[[ "$NC" -o -n "$ANDROID_SOCKET_adbd" ]] && {
   G=''; R=''; Y=''; B=''; V=''; Bl=''; C=''; W=''; N=''; BGBL=''; loadBar='=';
 }
 
@@ -176,22 +177,22 @@ div="${Bl}$(printf '%*s' "${character_no}" '' | tr " " "=")${N}"
 # title_div [-c] <title>
 # based on $div with <title>
 title_div() {
-  [ "$1" == "-c" ] && local character_no=$2 && shift 2
-  [ -z "$1" ] && { local message=; no=0; } || { local message="$@ "; local no=$(echo "$@" | wc -c); }
-  [ $character_no -gt $no ] && local extdiv=$((character_no-no)) || { echo "Invalid!"; return; }
+  [[ "$1" == "-c" ]] && local character_no=$2 && shift 2
+  [[ -z "$1" ]] && { local message=; no=0; } || { local message="$@ "; local no=$(echo "$@" | wc -c); }
+  [[ $character_no -gt $no ]] && local extdiv=$((character_no-no)) || { echo "Invalid!"; return 1; }
   echo "${W}$message${N}${Bl}$(printf '%*s' "$extdiv" '' | tr " " "=")${N}"
 }
 
 # set_file_prop <property> <value> <prop.file>
 set_file_prop() {
-  if [ -f "$3" ]; then
+  if [[ -f "$3" ]]; then
     if grep -q "$1=" "$3"; then
       sed -i "s/${1}=.*/${1}=${2}/g" "$3"
     else
       echo "$1=$2" >> "$3"
     fi
   else
-    echo "$3 doesn't exist!"
+    echo "$3 doesn't exist!"; return 1
   fi
 }
 
@@ -240,7 +241,7 @@ printf "\r${@} [${_indicator}]"
 e_spinner() {
   PID=$!
   h=0; anim='-\|/';
-  while [ -d /proc/$PID ]; do
+  while [[ -d /proc/$PID ]]; do
     h=$(((h+1)%4))
     sleep 0.02
     printf "\r${@} [${anim:$h:1}]"
@@ -252,11 +253,11 @@ e_spinner() {
 test_connection() {
   (
   if ping -q -c 1 -W 1 google.com >/dev/null 2>&1; then
-    internet=true
+    true
   elif ping -q -c 1 -W 1 baidu.com >/dev/null 2>&1; then
-    internet=true
+    true
   else
-    internet=false
+    false
   fi & e_spinner "Testing internet connection"
   ) && echo " - OK" || { echo " - Error"; false; }
 }
@@ -268,12 +269,12 @@ upload_logs() {
   $BBok && {
     test_connection || exit
     echo "Uploading logs"
-    [ -s $VERLOG ] && verUp=$(cat $VERLOG | nc termbin.com 9999) || verUp=none
-    [ -s $oldVERLOG ] && oldverUp=$(cat $oldVERLOG | nc termbin.com 9999) || oldverUp=none
-    [ -s $LOG ] && logUp=$(cat $LOG | nc termbin.com 9999) || logUp=none
-    [ -s $oldLOG ] && oldlogUp=$(cat $oldLOG | nc termbin.com 9999) || oldlogUp=none
-    [ -s $stdoutLOG ] && stdoutUp=$(cat $stdoutLOG | nc termbin.com 9999) || stdoutUp=none
-    [ -s $oldstdoutLOG ] && oldstdoutUp=$(cat $oldstdoutLOG | nc termbin.com 9999) || oldstdoutUp=none
+    [[ -s $VERLOG ]] && verUp=$(cat $VERLOG | nc termbin.com 9999) || verUp=none
+    [[ -s $oldVERLOG ]] && oldverUp=$(cat $oldVERLOG | nc termbin.com 9999) || oldverUp=none
+    [[ -s $LOG ]] && logUp=$(cat $LOG | nc termbin.com 9999) || logUp=none
+    [[ -s $oldLOG ]] && oldlogUp=$(cat $oldLOG | nc termbin.com 9999) || oldlogUp=none
+    [[ -s $stdoutLOG ]] && stdoutUp=$(cat $stdoutLOG | nc termbin.com 9999) || stdoutUp=none
+    [[ -s $oldstdoutLOG ]] && oldstdoutUp=$(cat $oldstdoutLOG | nc termbin.com 9999) || oldstdoutUp=none
     echo -n "Link: "
     echo "$MODEL ($DEVICE) API $API\n$ROM\n$ID\n
     O_Verbose: $oldverUp
@@ -285,7 +286,7 @@ upload_logs() {
     O_Log: $oldlogUp
     Log:   $logUp" | nc termbin.com 9999
   } || echo "Busybox not found!"
-  exit
+  exit 1
 }
 
 # Print Random
@@ -295,8 +296,8 @@ upload_logs() {
 prandom() {
   local CHANCES=2
   local TARGET=2
-  [ "$1" ==  "-c" ] && { local CHANCES=$2; local TARGET=$3; shift 3; }
-  [ "$((RANDOM%CHANCES+1))" -eq "$TARGET" ] && echo "$@"
+  [[ "$1" ==  "-c" ]] && { local CHANCES=$2; local TARGET=$3; shift 3; }
+  [[ "$((RANDOM%CHANCES+1))" -eq "$TARGET" ]] && echo "$@"
 }
 
 # Print Center
