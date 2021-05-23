c67c5be9df42031ee3165f7927c4e8357cb815b5
Pedrozzz0 <guitopzika26@gmail.com>
2021-05-23 14:36:56 -0300

    kingauto
    Signed-off-by: Pedrozzz0 <guitopzika26@gmail.com>

--- kcal.sh
+++ kcal.sh
@@ -1,8 +1,9 @@
 #!/system/bin/sh
-# KTSR by pedro (pedrozzz0 @ GitHub)
+# KTSR by Pedro (pedrozzz0 @ GitHub)
+# If you wanna use it as part of your project, please maintain the credits to it respective's author(s).
 
-if [[ -e "/sdcard/KTSR/kxvision.log" ]]; then
-rm "/sdcard/KTSR/kxvision.log"
+if [[ -e "/sdcard/KTSR/kcalkxvision.log" ]]; then
+rm "/sdcard/KTSR/kcalkxvision.log"
 fi
 
 # King Xvision 
@@ -13,9 +14,9 @@ echo "253" > "/sys/devices/platform/kcal_ctrl.0/kcal_val"
 echo "258" > "/sys/devices/platform/kcal_ctrl.0/kcal_cont"
 
 if [[ $? == "1" ]]; then
-echo "[!] Kcal preset executed with errors." > /sdcard/KTSR/kxvision.log
+echo "[!] Kcal preset executed with errors." > /sdcard/KTSR/kcalkxvision.log
 exit 1
 else
-echo "[*] Kcal preset executed without any errors! Enjoy." > /sdcard/KTSR/kxvision.log
+echo "[*] Kcal preset executed without any errors! Enjoy." > /sdcard/KTSR/kcalkxvision.log
 exit 0
 fi
