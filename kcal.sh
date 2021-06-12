eb4e29b287e17bbcc49d1ea0a990f79a34350cf3
Pedrozzz0 <guitopzika26@gmail.com>
2021-06-12 03:40:44 -0300

    Bump to v2.0.2 beta 3
    Signed-off-by: Pedrozzz0 <guitopzika26@gmail.com>

--- kcal.sh
+++ kcal.sh
@@ -2,7 +2,7 @@
 # KTSR by pedro (pedrozzz0 @ GitHub)
 
 if [[ -e "/sdcard/KTSR/kxvision.log" ]]; then
-rm "/sdcard/KTSR/kxvision.log"
+    rm "/sdcard/KTSR/kxvision.log"
 fi
 
 # King Xvision 
@@ -13,9 +13,10 @@ echo "253" > "/sys/devices/platform/kcal_ctrl.0/kcal_val"
 echo "258" > "/sys/devices/platform/kcal_ctrl.0/kcal_cont"
 
 if [[ $? == "1" ]]; then
-echo "[!] Kcal preset executed with errors." > /sdcard/KTSR/kxvision.log
-exit 1
+    echo "[!] Kcal preset executed with errors." > /sdcard/KTSR/kxvision.log
+    exit 1
+    
 else
-echo "[*] Kcal preset executed without any errors! Enjoy." > /sdcard/KTSR/kxvision.log
-exit 0
+    echo "[*] Kcal preset executed without any errors! Enjoy." > /sdcard/KTSR/kxvision.log
+    exit 0
 fi
